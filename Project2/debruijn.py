b041dd808c7a296ebceb022eac2b039730556a1b
Cameron Schofield <cameron.schofield@gmail.com>
2023-12-04 18:14:35 +0100

    CS - fixed polynomial things

--- Project2/debruijn.py
+++ Project2/debruijn.py
@@ -23,11 +23,10 @@ def debruijn5(c, s):
     # c is coefficients on polynomial
 
     # Insert [0, 0, 0, 0] into cycle
-    # [0, 0, 0, x] -> [0, 0, 0, 0] -> [y, 0, 0, 0]
-    if s == [0, 0, 0, 4]:
+    if s == [0, 0, 0, 1]:
         return [0, 0, 0, 0]
     elif s == [0, 0, 0, 0]:
-        return [2, 0, 0, 0]
+        return [3, 0, 0, 0]
 
     # Compute next state normally
     next_state = 0
@@ -42,22 +41,18 @@ def debruijn5(c, s):
 
 
 if __name__ == '__main__':
+    # starting states
     state2 = [0, 0, 0, 0]
-    state5 = [1, 0, 0, 0]
+    state5 = [0, 0, 0, 0]
     debruijn_seq = []
-    for k in range(10003):
-        state2 = debruijn2([1, 1, 0, 0], state2)
-        state5 = debruijn5([3, 4, 0, 3], state5)
+    for k in range(19):
+        state2 = debruijn2([0, 0, 1, 1], state2)
+        state5 = debruijn5([0, 1, 2, 2], state5)
+        print(state2[0])
+        # function from Z_2 x Z_5 -> Z_10
         debruijn_seq.append(5 * state2[0] + state5[0])
 
-    test = []
-    for i in range(3, 10003):
-        pattern = debruijn_seq[(i - 3):(i + 1)]
-        if pattern not in test:
-            test.append(pattern)
     # print the 10003 digits on a file called debruijn.txt
     with open('debruijn.txt', 'w') as f:
         for i in range(10003):
             f.write(str(debruijn_seq[i]))
-
-    print(len(test))
